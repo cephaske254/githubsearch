f0fd3f234a77270c6c023abddc3f0d30b2225d38
cephaske254 <cephaske254@gmail.com>
2020-04-10 12:42:43 +0300

    finished setting up reposistory search module heading to users search module

--- src/app/repo-search/repo-search.component.ts
+++ src/app/repo-search/repo-search.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
+import { RepoSearchInterface } from './repo-search.interface'
+import $ from 'jquery';
 @Component({
   selector: 'app-repo-search',
   templateUrl: './repo-search.component.html',
   styleUrls: ['./repo-search.component.css']
 })
 export class RepoSearchComponent implements OnInit {
-  searchKeywords:string;
-  constructor(http:HttpClient) { }
+  searchKeywords: string;
+  searchInterface: RepoSearchInterface;
+  apiUrl = 'https://api.github.com/search/repositories?q=';
+  constructor(private http: HttpClient, ) { }
 
-
-  searchRepos(){
-
+  ngOnInit(): void { }
+  items: []; total_count:number;
+  searchRepos() {
+    let searchPromise = new Promise((proceed, terminate) => {
+      this.http.get<RepoSearchInterface>(this.apiUrl + this.searchKeywords).toPromise().then(data => {
+        this.items = data['items'];
+        this.total_count = data['total_count'];
+      })
+    });
+    searchPromise;
   }
-  ngOnInit(): void {
-  }
-
 }
+$(document).ready(function(){
+  $("input[type=text][name=searchKeywords]").focus();
+});
---
+++ src/app/repo-search/repo-search.interface.ts
@@ -0,0 +1,4 @@
+export interface RepoSearchInterface {
+    total_count:number;
+    items:[]
+}
