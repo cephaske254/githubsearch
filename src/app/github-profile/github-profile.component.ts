ccbb0d12261ba2f2d237c0c5306b8b1439fd2488
cephaske254 <cephaske254@gmail.com>
2020-04-10 14:13:53 +0300

    created a api request to fetch repositories

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -8,13 +8,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GithubProfileComponent implements OnInit {
   apiUrl = 'https://api.github.com/users/cephaske254';
-  items:[]
+  items:any; repos:any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get(this.apiUrl).subscribe(data=>{
-      this.items = data['']
+      this.items = data;
+      this.http.get(data['repos_url']).subscribe(repos=>{
+        this.repos = repos;
+      })
     })
   }
 
