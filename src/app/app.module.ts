72a409fcbeb68a3bc924984703f2fea161a6776e
cephaske254 <cephaske254@gmail.com>
2020-04-10 11:16:05 +0300

    created repo-search and user-search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { Routes, RouterModule } from '@angular/router';
 
+import { FormsModule } from '@angular/forms';
+const appRoutes: Routes = [
+  { path: 'search/repos', component: RepoSearchComponent},
+  { path: 'search/users', component: UserSearchComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoSearchComponent,
+    UserSearchComponent,
+    GithubProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
