c0184ab4f8401409fc4fa898595002ff83819b08
cephaske254 <cephaske254@gmail.com>
2020-04-10 15:06:19 +0300

    added access_token

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -8,7 +8,8 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./github-profile.component.css']
 })
 export class GithubProfileComponent implements OnInit {
-  apiUrl = 'https://api.github.com/users/cephaske254?access_token='+environment.accesss_token+'';
+  access_token='839d7a02aff2db8b392082d629e69784f55f1880';
+  apiUrl = 'https://api.github.com/users/cephaske254?access_token='+this.access_token+'';
   items:any; repos:any;
 
   constructor(private http: HttpClient) { }
@@ -16,7 +17,7 @@ export class GithubProfileComponent implements OnInit {
   ngOnInit() {
     this.http.get(this.apiUrl).subscribe(data=>{
       this.items = data;
-      this.http.get(data['repos_url']+'?access_token='+environment.accesss_token).subscribe(repos=>{
+      this.http.get(data['repos_url']+'?access_token='+this.access_token).subscribe(repos=>{
         this.repos = repos;
       })
     })
